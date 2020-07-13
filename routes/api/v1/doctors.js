2abd5c88173eda03d51fe20998a41361a205efab
Shubham <shubhamnagpal30@gmail.com>
2020-07-13 11:09:40 +0530

    Add doctors route

---
+++ routes/api/v1/doctors.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// controller
+const DoctorController = require('../../../controller/api/v1/doctors');
+
+router.post("/register", DoctorController.register);
+router.post("/login", DoctorController.login);
+
+module.exports = router;
